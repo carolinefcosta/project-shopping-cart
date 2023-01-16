5f1ce6e88da1ff0169798dea4f8ae1025a55ffae
Caroline <carolinefernandesc@hotmail.com>
2023-01-16 17:02:27 -0300

    requisito 01 e 02

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,13 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (item) => {
+  const API = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
+  try {
+    const response = await fetch(API);
+    const result = await response.json();
+    return result.results;
+  } catch (error) {
+    throw new Error('Termo de busca não informado');
+  }
 };
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,26 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    fetchProductsList('computador');
+    await expect(fetch).toHaveBeenCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o result correto ao executar fetchProductsList', async () => {
+    fetchProductsList('computador');
+    const result = ('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+    await expect(fetch).toHaveBeenCalledWith(result);
   });
 
-  // it('...', () => {
-  // });
+  it('Teste se o retorno da função fetchProductsList com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    const response = await fetchProductsList('computador');
+    expect(response).toEqual(computadorSearch);
+  });
+
+  it('Teste se, ao chamar a função fetchProductsList sem argumento, retorna um erro.', async () => {
+    await expect(fetchProductsList()).rejects.toEqual(new Error('Termo de busca não informado'));
+  });
 });
